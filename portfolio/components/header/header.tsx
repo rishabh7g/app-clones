f9d9e4968f604b3d282cab9ced6773a1a73c1c83
Rishabh <rishabh7g@gmail.com>
2022-10-09 16:40:00 +0530

    Add header social icons

--- portfolio/components/header/header.tsx
+++ portfolio/components/header/header.tsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import { SocialIcon } from 'react-social-icons';
 
 export const Header = () => {
-   return <header>Header</header>;
+   return (
+      <header className='sticky top-0 p-5 flex items-start justify-between max-w-7xl mx-auto'>
+         <div>
+            <SocialIcon
+               url='https://www.youtube.com/rishabh7g'
+               fgColor='gray'
+               bgColor='transparent'
+            />
+            <SocialIcon
+               url='https://www.instagram.com/rishabh7g/'
+               fgColor='gray'
+               bgColor='transparent'
+            />
+            <SocialIcon
+               url='https://twitter.com/rishabh7g'
+               fgColor='gray'
+               bgColor='transparent'
+            />
+         </div>
+         <div className='flex flex-row items-center text-gray-300 cursor-pointer'>
+            <SocialIcon
+               className='cursor-pointer'
+               network='email'
+               fgColor='gray'
+               bgColor='transparent'
+            />
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>
+               Get in touch
+            </p>
+         </div>
+      </header>
+   );
 };
