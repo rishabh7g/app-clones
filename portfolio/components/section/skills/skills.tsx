7d0560d4e20fddf83feed147de5e8ff874b5f6f7
Rishabh <rishabh7g@gmail.com>
2022-10-12 11:31:51 +0530

    Add skills with hover effect

--- portfolio/components/section/skills/skills.tsx
+++ portfolio/components/section/skills/skills.tsx
@@ -1,5 +1,38 @@
+import { motion } from 'framer-motion';
+import { Skill } from '../../skill/skill';
+
 type Props = {};
 
 export const Skills = ({}: Props) => {
-   return <div className='h-screen'>Skills</div>;
+   return (
+      <motion.div
+         initial={{ opacity: 0 }}
+         whileInView={{ opacity: 1 }}
+         transition={{ duration: 1.5 }}
+         className='relative h-screen flex overflow-hidden flex-col text-lft md:flex-row max-w-full px-10 justify-evenly mx-auto items-center'
+      >
+         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>
+            Skills
+         </h3>
+         <h3 className='absolute top-36 uppercase tracking-[13px] text-gray-500 text-sm'>
+            Hover over a skill for current profeciency
+         </h3>
+         <div className='grid grid-cols-4 gap-5'>
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+            <Skill />
+         </div>
+      </motion.div>
+   );
 };
