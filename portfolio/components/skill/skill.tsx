f0b41975f8cdfa5769e201e30ab35539e4fb0542
Rishabh <rishabh7g@gmail.com>
2022-10-13 14:12:27 +0530

    Prettify class names

--- portfolio/components/skill/skill.tsx
+++ portfolio/components/skill/skill.tsx
@@ -5,6 +5,13 @@ interface SkillProps {
    directionLeft?: boolean;
 }
 
+// class conflicts
+
+// BEM - Block element modifier
+// Styled compnent
+// <div> <button class='btn'> </StyledDiv>
+// 
+
 export const Skill = ({ directionLeft, ...props }: SkillProps) => {
    return (
       <div {...props} className='group relative flex cursor-pointer'>
@@ -13,12 +20,13 @@ export const Skill = ({ directionLeft, ...props }: SkillProps) => {
             transition={{ duration: 1 }}
             whileInView={{ opacity: 1, x: 0 }}
             src='/assets/svg/css3.svg'
-            className='h-24 w-24 rounded-full border border-gray-500 object-cover filter transition duration-300 ease-in-out group-hover:grayscale xl:h-32 xl:w-32'
+            className='border-gray-500 h-24 w-24 rounded-full border object-cover filter transition duration-300 ease-in-out group-hover:grayscale xl:h-32 xl:w-32'
          />
-         <div className='absolute z-0 h-24 w-24 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:bg-white group-hover:opacity-80 md:h-28 md:w-28 xl:h-32 xl:w-32'>
+         <div className='group-hover:bg-white absolute z-0 h-24 w-24 rounded-full opacity-0 transition duration-300 ease-in-out group-hover:opacity-80 md:h-28 md:w-28 xl:h-32 xl:w-32'>
             <div className='flex h-full items-center justify-center'>
-               <p className='text-3xl font-bold text-black opacity-100'>100%</p>
+               <p className='text-black text-3xl font-bold opacity-100'>100%</p>
             </div>
+            <button className='bg-red text-red'>Click</button>
          </div>
       </div>
    );
